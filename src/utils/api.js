a52b207c4e76084842f25d2280e0f768119bc687
Dikshit <dikshitmaheshwari15@gmail.com>
2019-11-21 02:27:41 +0530

    changed API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
-const api = "http://127.0.0.1:5000/";
-// const api = "https://thawing-eyrie-73297.herokuapp.com/"
+// const api = "http://127.0.0.1:5000/";
+const api = "https://tapchief-dikshit-server.herokuapp.com/"
 export default api;
